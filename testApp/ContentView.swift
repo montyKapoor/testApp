ec5ff9fff4fa40d6c93b9307bc66f8ab4a3dfe3e
abhizop <abhishek.g@zopsmart.com>
2022-11-16 16:44:00 +0530

    Initial Commit

---
+++ testApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testApp
+//
+//  Created by Abhishek Gupta on 16/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testApp/testAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  testAppApp.swift
+//  testApp
+//
+//  Created by Abhishek Gupta on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct testAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
